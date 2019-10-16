aa2e2e4a5a4a4ba2e82461ae599f799cd397e5bf
seyed roohollah marashi <srmarashi@yahoo.com>
2019-10-17 00:51:15 +0330

    Add files via upload

---
+++ arduinolm324voltmeter.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include "U8glib.h"
+U8GLIB_PCD8544 u8g(2, 3, 5, 4,6);  // CLK ,DIN ,CE ,DC ,RST  
+
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(7, INPUT);
+  digitalWrite(11,HIGH);
+  Serial.begin(9600);
+
+      
+}
+
+void loop() {  
+  u8g.firstPage(); do {      
+       u8g.setFont(u8g_font_profont12);
+        float v = analogRead(A0);
+         Serial.println(v * 0.01165630 );
+       u8g.setPrintPos( 0, 7); 
+       u8g.print("Easy Meter ");
+       u8g.setPrintPos( 0, 24); 
+       u8g.print(v*0.01165630);
+       u8g.setPrintPos( 0, 37); 
+       u8g.print("VOLT");
+       
+       } 
+      while( u8g.nextPage() );
+}
